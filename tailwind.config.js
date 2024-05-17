7e9001fa211bec4116c65477e1090d7fc33aa423
MoonbamiOfficial <141120384+MoonbamiOfficial@users.noreply.github.com>
2024-05-17 23:05:56 +0800

    config: add project custom styling properties

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      primary: "#76ABAE",
+      "primary-variant": "#54898A",
+      background: "#222831",
+      surface: "#31363F",
+      error: "#C12A3E",
+    },
+    extend: {
+      fontFamily: {
+        poppins: [
+          "Poppins",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      container: {
+        center: true,
+      },
+      screens: {
+        xs: "475px",
+        "3xl": "1920px",
+      },
+    },
   },
   plugins: [],
 };
